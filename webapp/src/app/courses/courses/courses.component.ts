236ae44b4fe1b8114cca7ec7fcaaeb67650ffaa3
Caio <caiofellipe80@gmail.com>
2021-12-10 22:59:15 -0300

    create list courses and create color palette

---
+++ webapp/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    { id: '1', name: 'Angular', category: 'Front-end' }
+  ]; 
+  displayedColumns = ['name', 'category'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
